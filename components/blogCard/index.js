3a435ea952d1bbb95c04a4060cfb4bbfa0e77b84
Langford Kwabena <55645613+langfordquarshie21@users.noreply.github.com>
2022-06-20 01:14:13 +0000

    fixed blog card width

--- components/blogCard/index.js
+++ components/blogCard/index.js
@@ -7,10 +7,10 @@ const BlogCard = ({ post }) => {
             <li className="border-b border-b-borderGray lg:pb-10 cursor-pointer lg:mb-8 pb-16 mb-16 blog-card">
                 <Link href={`/read?${post.search_title}`}>
                     <div className='flex items-center flex-col lg:flex-row justify-between'>
-                        <div className='max-w-3xl'>
+                        <div className='w-full lg:max-w-3xl'>
                             <b className='lg:text-2xl text-xl font-bold'>{post.title}</b>
                             <div className='opacity-50'><small>{post.timestamp} | {post.read_length} min read | {post.views} views</small></div>
-                            <p className='mt-5 opacity-70 multiline--6'>{post.content}</p>
+                            <p className='mt-5 opacity-70 multiline--4'>{post.content}</p>
                         </div>
                         <img className='w-full mt-5 lg:w-[320px]' src={post.banner} alt='blog title' />
                     </div>
