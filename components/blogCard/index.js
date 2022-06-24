35560a2a362575938c975024a39fe64e5eba4c8a
Langford Kwabena <55645613+langfordquarshie21@users.noreply.github.com>
2022-06-24 00:59:41 +0000

    update

--- components/blogCard/index.js
+++ components/blogCard/index.js
@@ -1,35 +1,13 @@
 import Link from "next/link"
-import Image from "next/image"
-
-// /* eslint-disable @next/next/no-img-element */
-// const BlogCard = ({ article }) => {
-//     return (
-//         <>
-//             <li className="border-b border-b-borderGray lg:pb-10 cursor-pointer lg:mb-8 pb-16 mb-16 blog-card">
-//                 <Link href={`/read?${article.search_title}`}>
-//                     <div className='flex items-center flex-col lg:flex-row justify-between'>
-//                         <div className='w-full lg:max-w-3xl'>
-//                             <b className='lg:text-2xl text-xl font-bold'>{article.title}</b>
-//                             <div className='opacity-50'><small>{article.timestamp} • {article.read_length} min read • {article.views} views</small></div>
-//                             <p className='mt-5 opacity-50 leading-[30px] multiline--4'>{article.content}</p>
-//                         </div>
-//                         <img className='w-full mt-5 lg:w-[320px] rounded-md border border-borderGray' src={article.banner} alt={article.title} />
-//                     </div>
-//                 </Link>
-//             </li>
-//         </>
-//     )
-// }
-
 
 const BlogCard = ({ article }) => {
     return (
         <>
             <Link href={`/read?${article.search_title}`}>
                 <div className="blog-card w-full cursor-pointer lg:p-3 lg:-ml-3 lg:mb-10 mb-14">
-                    <img src={article.banner} alt={article.title} className='rounded-[20px]' />
+                    <img src={article.banner} alt={article.title} className='rounded-[10px]' />
                     <div className="mt-5">
-                        <p className="my-1 text-xl">{article.title}</p>
+                        <p className="my-1 lg:text-xl text-[17px]">{article.title}</p>
                         <div className='opacity-50'>
                             {article.timestamp} • {article.read_length} min read
                         </div>
--- components/drawer/index.js
+++ components/drawer/index.js
@@ -4,6 +4,7 @@ import Link from "next/link"
 import Button from "../button"
 import { styles } from "./styles"
 import { modalStyles } from "../styles/modal.styles"
+import { tagTabs } from "../../content"
 
 const Drawer = () => {
     const { showDrawer } = useContext(BlogContext)
@@ -11,11 +12,20 @@ const Drawer = () => {
     if (showDrawer) return (
         <section className={modalStyles.drawerWrapper}>
             <div className={styles.drawer}>
-                <Link href='/' passHref><p className={styles.link}>Home</p></Link>
+                {tagTabs.map((tag, i) => {
+                    return <Link passHref key={i} href={`/tag?${tag.toLocaleLowerCase().replace(' ', '-')}`}>
+                        <p className={styles.link}>{tag}</p>
+                    </Link>
+                })}
+
+                <div className="m-5" />
+                <Button page='/write' label='Write a blog' />
+
+                {/* <Link href='/' passHref><p className={styles.link}>Home</p></Link>
                 <Link href='/' passHref><p className={styles.link}>About</p></Link>
                 <Link href='/' passHref><p className={styles.link}>Posts</p></Link>
-                <Link href='/' passHref><p className={styles.link}>About</p></Link>
-                <Button page='/write' label='Write a blog' />
+                <Link href='/' passHref><p className={styles.link}>About</p></Link> */}
+
             </div>
         </section>
     )
