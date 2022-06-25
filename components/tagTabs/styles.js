700c6a18a4d4aafe39b3dba26240c81da7fb2068
Langford Kwabena <55645613+langfordquarshie21@users.noreply.github.com>
2022-06-25 08:30:49 +0000

    removed bg color

--- components/tagTabs/styles.js
+++ components/tagTabs/styles.js
@@ -1,5 +1,5 @@
 export const styles = {
-    tabWrapper: `pt-10 -mt-10 border-b border-b-borderGray bg-[#f5f5f5de] w-screen`,
+    tabWrapper: `pt-10 -mt-10 border-b border-b-borderGray w-screen`,
     tabContainer: `flex items-center max-w-6xl m-auto overflow-x-scroll`,
     link: `border text-[#707070] bg-[#fff] border-borderGray w-max py-[8px] px-3 rounded-md ml-5 mb-5 text-sm hover:bg-[#000] hover:text-[#fff] cursor-pointer`,
     // link: `p-5 px-2 mx-2 whitespace-nowrap cursor-pointer hover:text-brand transition hover:border-b-2 hover:border-b-brand border-b-2 border-b-[#fff]`,
